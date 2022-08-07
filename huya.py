4c24a7a849170e1d7ea217e5e3a18ade3d599c37
Angelo Hamlet <francojia@outlook.com>
2022-08-07 18:10:57 +0800

    Update huya.py

--- huya.py
+++ huya.py
@@ -1,44 +1,75 @@
-from bs4 import BeautifulSoup
-import requests
 import json
+from typing import *
 
-def huya(url:str) -> str:
-    #get content from url with user-agent and store in htmlDoc
-    headers = {'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'}
-    htmlDoc = requests.get(url, headers=headers)
-    #parse htmlDoc with BeautifulSoup
-    soup = BeautifulSoup(htmlDoc.text, 'html.parser')
-    a = soup.body.contents[8]
-    #replace '<script> window.HNF_GLOBAL_INIT = ' with empty string
-    b = str(a).replace('<script> window.HNF_GLOBAL_INIT = ', '')
-    #replace '</script>' with empty string
-    c = str(b).replace('</script>', '')
-    #get the json content and decode it
-    jsonContent = json.loads(c)
-    #get the roomValue and roomIntroduction from the json content
-    eLiveStatus = jsonContent['roomInfo']['eLiveStatus']
-    if eLiveStatus == 2:
-        roomValue = jsonContent['roomInfo']['tLiveInfo']['tLiveStreamInfo']['vStreamInfo']['value']
-        roomIntroduction = jsonContent['roomInfo']['tLiveInfo']['sIntroduction']
-        #create a loop to store all cdnType into a list
-        cdnTypeList = []
-        for i in range(0, 3):
-            cdnType = roomValue[int(i)]['sCdnType']     
-            #store cdnType in cdnTypeList
-            cdnTypeList.append(str(i) + cdnType)
-        #ask user to input the cdn number
-        cdnNumber = (input(roomIntroduction + str(cdnTypeList) + 'Please input the cdn type number: '))
-        #get parameter
-        sStreamName = roomValue[int(cdnNumber)]['sStreamName']
-        sFlvUrl = roomValue[int(cdnNumber)]['sFlvUrl']
-        sFlvAntiCode = roomValue[int(cdnNumber)]['sFlvAntiCode']
+import requests
+from bs4 import BeautifulSoup
+
+
+def huyaNew(room_id: str) -> str:
+    try:
+        if not room_id.isdigit():
+            try:
+                headers = {
+                    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'}
+                htmlDoc = requests.get(
+                    f'https://m.huya.com/{room_id}', headers=headers)
+                soup = BeautifulSoup(htmlDoc.text, 'html.parser')
+                a = soup.body.contents[8]
+                b = str(a).replace('<script> window.HNF_GLOBAL_INIT = ', '')
+                c = str(b).replace('</script>', '')
+                jsonContent = json.loads(c)
+                room_id = str(jsonContent['roomInfo']
+                              ['tProfileInfo']['lProfileRoom'])
+                print(
+                    f'Your room id is invalid! The real room id is: {room_id}')
+            except:
+                print('Room id is invalid!')
+                return
+        api_url = 'https://mp.huya.com/cache.php?m=Live&do=profileRoom&roomid='
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'}
+        html = requests.get(api_url + room_id, headers=headers)
+        data = json.loads(html.text)['data']
+
+        nick = data['profileInfo']['nick']
+        room_name = data['liveData']['roomName']
+        liveStatus = data['liveStatus']
+    except:
+        print('Room id is invalid!')
+        return
     
-        #conbine the url
-        finalUrl = sFlvUrl + '/' + sStreamName + '.flv?' + sFlvAntiCode
-                
+
+    if liveStatus == "OFF":
+        return f'{room_name} {nick} is offline'
     else:
-        finalUrl = 'Live Unavailable'
+        stream_dict = data['stream']
+        flv = stream_dict['flv']  # type: dict
+        cdn = flv['multiLine']  # type: List
+        rate = flv['rateArray']  # type: List
+        supportable_resolution = {'原画': '', }
+        for b in rate:
+            supportable_resolution[b['sDisplayName']] = b['iBitRate']
+        sort_dict = {}
+        for resolution, bitrate in supportable_resolution.items():
+            print(f'{resolution} {bitrate}')
+            url_list = []
+            for i in cdn:
+                url = i['url'].replace('http://', 'https://')  # type: str
+                url = url.replace(
+                    'imgplus.flv', f'imgplus_{bitrate}.flv')  # type: str
+                url_list.append(url)
 
-    return finalUrl
+            sort_dict[resolution] = url_list
+        display = ''
+        for resolution, url_list in sort_dict.items():
+            display += f'{resolution}:\n'
+            for i in url_list:
+                display += f'{i}\n'
+    return f'{nick} is online \n{room_name} \n{display}'
 
-print(huya('https://m.huya.com/243547'))
+
+if __name__ == '__main__':
+    room_id = input('Please input the room id: ')
+    if room_id == '':
+        print(huyaNew('243547'))
+    print(huyaNew(room_id))
